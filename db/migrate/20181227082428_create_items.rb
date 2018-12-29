6df9162e4f8a99c7ad8666cabee3465112a3fb4c
rishabh1998 <gamerishabh10@gmail.com>
2018-12-29 12:12:52 +0530

    view added

---
+++ db/migrate/20181227082428_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.references :category, foreign_key: true
+      t.references :department, foreign_key: true
+      t.string :name
+      t.string :description
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
